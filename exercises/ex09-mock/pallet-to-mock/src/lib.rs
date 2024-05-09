04625fd45be8ac973a0ee534c235c59f46ed3b0f
Abhijana <abhijanaramanda@gmail.com>
2024-05-09 19:47:35 +0800

    make ex09 starter compile

--- exercises/ex09-mock/pallet-to-mock/src/lib.rs
+++ exercises/ex09-mock/pallet-to-mock/src/lib.rs
@@ -23,7 +23,7 @@ pub mod pallet {
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
 		type Currency: Currency<Self::AccountId>;
 
@@ -34,7 +34,6 @@ pub mod pallet {
 	}
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
 	#[pallet::event]
